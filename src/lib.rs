5f2e0a8731af770fded5e51e6e6c2957bd6f806c
Immad Mir <mirimmad17@gmail.com>
2024-12-24 16:43:41 +0530

    mac: add support using diskutil

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
-use std::{env, path::PathBuf};
+use std::process::{Command, Stdio};
+use std::{env, path::PathBuf, str::FromStr};
 use sysinfo::Disks;
 
 #[derive(Debug, Default)]
 pub struct DiskDevice {
-    list: Vec<PathBuf>,
+    pub list: Vec<PathBuf>,
 }
 
 pub fn get_disk_devices() -> DiskDevice {
     let mut disks: DiskDevice = DiskDevice::default();
     disks.list = match env::consts::OS {
         "linux" => get_disk_devices_linux(),
-        _ => vec![],
+        "macos" => get_disk_devices_mac(),
+        _ => panic!("Unknown OS"),
     };
 
     return disks;
@@ -26,14 +28,45 @@ fn get_disk_devices_linux() -> Vec<PathBuf> {
     return out;
 }
 
+fn get_disk_devices_mac() -> Vec<PathBuf> {
+    let diskutil_output = Command::new("diskutil")
+        .arg("list")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("diskutil failed to execute");
+
+    let grep_output = Command::new("grep")
+        .arg("/dev/disk")
+        .stdin(diskutil_output.stdout.unwrap())
+        .output()
+        .expect("grep failed to execute");
+
+    let out = String::from_utf8_lossy(&grep_output.stdout);
+    let lines: Vec<&str> = out.as_ref().split('\n').collect();
+    let disks: Vec<Vec<&str>> = lines
+        .into_iter()
+        .map(|line| line.split_whitespace().collect())
+        .collect();
+    let mut paths = Vec::new();
+    for disk in disks {
+        let path = disk.get(0);
+        if !path.is_some() {
+            continue;
+        }
+        paths.push(PathBuf::from_str(path.unwrap()).unwrap());
+    }
+    paths
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[cfg(target_os = "linux")]
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
     #[test]
     fn linux_test() {
         let result = get_disk_devices();
+        //println!("{:?}", result.list);
         assert!(result.list.len() > 0);
     }
 }
